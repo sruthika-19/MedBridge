2ea10d9fc6dac5cb71b2157d6eae18f103d3d9c7
sruthika-19 <sruthika1907@gmail.com>
2026-08-19 01:24:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from search_engine import search_disease
+from fastapi import File, UploadFile
+from fastapi.responses import StreamingResponse
+import csv
+import io
 import datetime
 
 app = FastAPI(title = "MedBridge API")
+class MappingRequest(BaseModel):
+    term: str
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,7 +33,61 @@ def home():
     summary="Search Traditional Term",
     tags=["EMR Integration"]
 )
+
 def get_icd_code(disease_name: str, background_tasks: BackgroundTasks):
     background_tasks.add_task(log_audit, disease_name)
     result = search_disease(disease_name)
     return result
+
+@app.post("/api/v1/map")
+def map_term(request: MappingRequest, background_tasks: BackgroundTasks):
+    background_tasks.add_task(
+        log_audit,
+        request.term
+    )
+    result = search_disease(request.term)
+    return result
+
+@app.post("/api/v1/bulk-map", tags=["Enterprise Features"])
+async def bulk_map_csv(file: UploadFile = File(...)):
+    content = await file.read()
+    text = content.decode("utf-8")
+    
+    output_csv = io.StringIO()
+    writer = csv.writer(output_csv)
+    
+    writer.writerow(["Original_Diagnosis", "Status", "NAMASTE_Code", "ICD_11_Code", "Confidence", "System"])
+    
+    reader = csv.reader(io.StringIO(text))
+    next(reader, None)
+    
+    success_count = 0
+    fail_count = 0
+
+    for row in reader:
+        if not row: continue
+        term = row[0].strip()
+        result = search_disease(term)
+        
+        if result["status"] == "success":
+            fhir = result["data"][0]
+            namaste = fhir["code"]["coding"][0]["code"]
+            icd = fhir["code"]["coding"][1]["code"]
+            conf = fhir["confidenceScore"]
+            sys_name = fhir["code"]["coding"][0]["system"].split(":")[-1].upper()
+            
+            writer.writerow([term, "Mapped", namaste, icd, conf, sys_name])
+            success_count += 1
+        else:
+            writer.writerow([term, "Failed (Needs Manual Review)", "N/A", "N/A", "N/A", "N/A"])
+            fail_count += 1
+
+    output_csv.seek(0)
+    
+    print(f"Bulk Processed: {success_count} mapped, {fail_count} failed.")
+    
+    return StreamingResponse(
+        output_csv, 
+        media_type="text/csv",
+        headers={"Content-Disposition": f"attachment; filename=MedBridge_Standardized_{file.filename}"}
+    )
